bcda5060afb13e0c54cf3f73a8dba765d78c56d6
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-08-20 22:43:40 +0900

    smcc

--- script.js
+++ script.js
@@ -8,7 +8,18 @@ function setup() {
 }
 function draw() {
   //オブジェクトの色
-  fill(random(255), random(255), random(255), 50);
+  fill(0);
   //キャンバスの中心に直径100pxの丸を描画
-  ellipse(random(width), random(height), 100);
+  // ellipse(random(), random(height), 100);
+  const cx = 50;
+  const cy = 50;
+  const d = 10;
+
+  rect(cx, cy, 1, 1);
+
+  for (let i = 0; i < d; i++) {
+    for (let j = 0; j < d; j++) {
+      rect(Math.abs(i + d), j, 1, 1);
+    }
+  }
 }
