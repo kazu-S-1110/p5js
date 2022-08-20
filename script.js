94449303fd6c4788a48111cdb0eb18b23ddeecf2
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-08-20 23:14:24 +0900

    2次関数を描画。

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function setup() {
   //キャンバスを作成
-  createCanvas(600, 425);
+  createCanvas(600, 600);
   //背景色
   background(255);
 
@@ -10,31 +10,30 @@ function draw() {
   //オブジェクトの色
   //キャンバスの中心に直径100pxの丸を描画
   // ellipse(random(), random(height), 100);
-  const cx = 50;
-  const cy = 50;
-  const d = 10;
+  const cx = 300;
+  const cy = 300;
+  const d = 600;
   fill("black");
 
-  for (let x = -50; x < 50; x++) {
+  for (let x = -cx; x < cy; x++) {
     rect(cx + x, cy, 1, 1);
   }
 
   fill("black");
-  for (let y = -50; y < 50; y++) {
+  for (let y = -cx; y < cy; y++) {
     rect(cx, cy + y, 1, 1);
   }
 
   // 一次関数
   // fill("blue");
-  // for (let a = 0; a < 100; a++) {
-  //   rect(a, 100 - a, 1, 1);
+  // for (let a = 0; a < d; a++) {
+  //   rect(a, d - a, 1, 1);
   // }
 
   // 二次関数
   fill("red");
-  for (let b = 0; b < 100; b++) {
-    for (let c = 0; c < 30; c++) {
-      rect(50 + b, 50 - b + c ** 2, 1, 1);
-    }
+  for (let b = 0; b < d; b++) {
+    rect(cx + b, cy - (b / 8) ** 2, 1, 1);
+    rect(cx - b, cy - (b / 8) ** 2, 1, 1);
   }
 }
