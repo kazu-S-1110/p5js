ef4f758eabb2256e20090b0bbd7c3a8d82285a57
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-08-21 20:40:44 +0900

    円の関数式を改良。

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 function setup() {
   //キャンバスを作成
-  createCanvas(600, 600);
+  const wHeight = windowHeight;
+  const wWidth = windowWidth;
+  createCanvas(wWidth, wHeight);
   //背景色
   background(255);
 
   noStroke();
 }
 function draw() {
+  const wHeight = windowHeight;
+  const wWidth = windowWidth;
   //オブジェクトの色
   //キャンバスの中心に直径100pxの丸を描画
   // ellipse(random(), random(height), 100);
-  const cx = 300;
-  const cy = 300;
+  const cx = wWidth / 2;
+  const cy = wHeight / 2;
   const d = 200;
 
   fill("black");
@@ -38,8 +42,13 @@ function draw() {
   //   rect(cx - b, cy - (b / 8) ** 2, 1, 1);
   // }
 
+  // マウス座標に円
+  // clear();
+  // circle(mouseX, mouseY, 100);
+
+  // 円の関数
   fill("green");
-  for (let c = 0; c < d; c++) {
+  for (let c = 0; c < d; c = c + 0.1) {
     const divide = 10;
     rect(
       cx + Math.cos(c / divide) * 100,
